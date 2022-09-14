a47fecdad8cfa69a92385a7395c73df627cdd199
Tobi Aderibigbe <0xdegn@gmail.com>
2022-09-14 11:50:05 +0100

    Renamed week2 to React-hooks

--- 2nd Semester/month-2/week-1/React-hooks/src/App.js
+++ 2nd Semester/month-2/week-1/React-hooks/src/App.js
@@ -1,8 +1,22 @@
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+
+function Welcome() {
+  const [welcome, setWelcome] = useState(
+    "Welcome User, you'll be logged in in a few minutes"
+  );
+
+  setTimeout(() => {
+    setWelcome("You're logged in");
+  }, 5000);
+
+  return <h1>{welcome}</h1>;
+}
 
 function App() {
   return (
     <div className="App">
+      <Welcome />
     </div>
   );
 }
