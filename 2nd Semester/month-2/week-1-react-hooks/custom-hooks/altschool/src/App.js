8b339be110df4fea2a6b34a6a2355598cd6d47d3
Tobi Aderibigbe <0xdegn@gmail.com>
2022-09-19 15:29:28 +0100

    Updated App.js

--- 2nd Semester/month-2/week-1-react-hooks/custom-hooks/altschool/src/App.js
+++ 2nd Semester/month-2/week-1-react-hooks/custom-hooks/altschool/src/App.js
@@ -2,14 +2,25 @@ import "./App.css";
 import React from "react";
 import useGetData from "./Hooks/useCustomHooks";
 
-
 function App() {
   const data = useGetData("cars");
-  console.log(data)
- 
-  return <div className="App">
-    
-  </div>;
+
+  return (
+    <div className="App">
+      {data.map((car) => {
+        return (
+          <div className="car-container">
+            <div key={car.name} className = "car-card">
+              <h1>Car Details</h1>
+              <p>{car.name}</p>
+              <p>{car.year}</p>
+              <p>{car.price}</p>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default App;
